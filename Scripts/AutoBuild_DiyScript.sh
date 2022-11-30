6ed8042d4ec687605d506ac253bac13e0373c990
纪台伟(智能网联开发院-人工智能研究所) <jitaiwei@faw.com.cn>
2022-11-30 14:17:06 +0800

    beta

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -7,13 +7,13 @@ Firmware_Diy_Core() {
 	Author=AUTO
 	Author_URL=AUTO
 	Default_Flag=AUTO
-	Default_IP="192.168.68.1"
+	Default_IP="192.168.1.1"
 	Default_Title="Powered by AutoBuild-Actions"
 
 	Short_Fw_Date=true
 	x86_Full_Images=false
 	Fw_Format=false
-	Regex_Skip="packages|buildinfo|sha256sums|manifest|kernel|factory|itb|profile|json"
+	Regex_Skip="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory|itb|profile|ext4|json"
 
 	AutoBuild_Features=true
 }
@@ -38,5 +38,64 @@ Firmware_Diy() {
 	# ${FEEDS_LUCI}			OpenWrt 源码目录下的 package/feeds/luci 目录
 	# ${FEEDS_PKG}			OpenWrt 源码目录下的 package/feeds/packages 目录
 	# ${BASE_FILES}			OpenWrt 源码目录下的 package/base-files/files 目录
-	:
+
+	case "${OP_AUTHOR}/${OP_REPO}:${OP_BRANCH}" in
+	coolsnowwolf/lede:master)
+		cat >> ${Version_File} <<EOF
+sed -i '/check_signature/d' /etc/opkg.conf
+if [ -z "\$(grep "REDIRECT --to-ports 53" /etc/firewall.user 2> /dev/null)" ]
+then
+	echo '# iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# [ -n "\$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '# [ -n "\$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+fi
+exit 0
+EOF
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
+		# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+		# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon-mod"' $(PKG_Finder d package default-settings)/files/zzz-default-settings
+		
+		for i in eqos mentohust minieap unblockneteasemusic
+		do
+			AddPackage svn apps luci-app-${i} immortalwrt/luci/branches/openwrt-18.06/applications
+			sed -i 's/..\/..\//\$\(TOPDIR\)\/feeds\/luci\//g' ${WORK}/package/apps/luci-app-${i}/Makefile
+		done ; unset i
+
+		rm -r ${FEEDS_LUCI}/luci-theme-argon*
+		AddPackage git themes luci-theme-argon jerrykuku 18.06
+		AddPackage svn apps minieap immortalwrt/packages/branches/openwrt-18.06/net
+		AddPackage svn other luci-app-openclash vernesong/OpenClash/branches/dev
+		AddPackage git lean luci-app-argon-config jerrykuku master
+		AddPackage git other luci-app-ikoolproxy iwrt main
+		AddPackage git other helloworld fw876 master
+		AddPackage git themes luci-theme-neobird thinktip main
+		AddPackage git other luci-app-smartdns pymumu lede
+
+		case "${TARGET_BOARD}" in
+		ramips)
+			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
+			Copy ${CustomFiles}/Depends/automount $(PKG_Finder d "package" automount)/files 15-automount
+		;;
+		esac
+
+		case "${TARGET_PROFILE}" in
+		d-team_newifi-d2)
+			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
+		;;
+		x86_64)
+			Copy ${CustomFiles}/Depends/cpuset ${BASE_FILES}/bin
+			AddPackage git passwall-depends openwrt-passwall xiaorouji packages
+			AddPackage git passwall-luci openwrt-passwall xiaorouji luci
+			rm -rf packages/lean/autocore
+			AddPackage git lean autocore-modify Hyy2001X master
+			sed -i -- 's:/bin/ash:'/bin/bash':g' ${BASE_FILES}/etc/passwd
+			# sed -i "s?6.0?5.19?g" ${WORK}/target/linux/x86/Makefile
+		;;
+		esac
+	;;
+	immortalwrt/immortalwrt*)
+		sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
+	;;
+	esac
 }
